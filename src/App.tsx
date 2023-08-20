b8f88e7c7c8115baf3f2d48ddc4a474c82fe783f
Jason Zhu <jasonzhuyq@outlook.com>
2023-08-20 18:21:37 +1000

    Added Handles on RequirementNode, and change initialNodes to RequirementNodes

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,31 @@ import ReactFlow, {
   EdgeChange,
   Node,
   NodeChange,
+  NodeTypes,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { useCallback, useState } from 'react';
 
+import RequirementNode from './components/RequirementNode.tsx';
+
 const initialNodes: Node[] = [
   {
     id: '1',
-    data: { label: 'Hello' },
+    data: { title: 'Req1', description: 'Description of Req1' },
     position: { x: 0, y: 0 },
-    type: 'input',
+    type: 'requirement',
   },
   {
     id: '2',
-    data: { label: 'World' },
+    data: { title: 'Req2', description: 'Description of Req2' },
     position: { x: 100, y: 100 },
+    type: 'requirement',
   },
 ];
 
 const initialEdges: Edge[] = [{ id: '1-2', source: '1', target: '2' }];
+
+const nodeTypes: NodeTypes = { requirement: RequirementNode };
 
 const Flow = () => {
   const [nodes, setNodes] = useState(initialNodes);
@@ -58,6 +64,7 @@ const Flow = () => {
         edges={edges}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
         fitView
       >
         <Background />
--- src/components/RequirementNode.tsx
+++ src/components/RequirementNode.tsx
@@ -1,20 +1,46 @@
 import React, { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
 
-const RequirementNode = () => {
+interface RequirementNodeDataProps {
+  data: {
+    id: string;
+    title: string;
+    description: string;
+  };
+  isConnectable: boolean;
+}
+
+const RequirementNode = ({ data, isConnectable }: RequirementNodeDataProps) => {
   const onChange = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
     console.log(evt.target.value);
   }, []);
   return (
     <div>
+      <Handle
+        type={'target'}
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
       <label htmlFor={'text'}>ID:</label>
       {/* ID from backend */}
       <br />
       <label htmlFor={'text'}>Title:</label>
-      <input id={'text'} type={'text'} onChange={onChange} />
+      <input id={'text'} type={'text'} onChange={onChange} value={data.title} />
       <br />
       <label htmlFor={'text'}>Description:</label>
-      <input id={'text'} type={'text'} onChange={onChange} />
+      <input
+        id={'text'}
+        type={'text'}
+        onChange={onChange}
+        value={data.description}
+      />
       <br />
+      <Handle
+        type={'source'}
+        position={Position.Bottom}
+        id="b"
+        isConnectable={isConnectable}
+      />
     </div>
   );
 };
