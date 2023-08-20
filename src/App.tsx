6630907f3ee882cb4e793cd9cf0bb99a173ad932
Jason Zhu <jasonzhuyq@outlook.com>
2023-08-20 14:29:27 +1000

    Implemented reactive reactflow with dumb data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import ReactFlow, { Background, Controls, Edge, Node } from 'reactflow';
+import ReactFlow, {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  EdgeChange,
+  Node,
+  NodeChange,
+} from 'reactflow';
 import 'reactflow/dist/style.css';
+import { useCallback, useState } from 'react';
 
 const initialNodes: Node[] = [
   {
@@ -17,26 +29,37 @@ const initialNodes: Node[] = [
 
 const initialEdges: Edge[] = [{ id: '1-2', source: '1', target: '2' }];
 
-const nodes: Node[] = [
-  {
-    id: '1',
-    data: { label: 'Hello' },
-    position: { x: 0, y: 0 },
-    type: 'input',
-  },
-  {
-    id: '2',
-    data: { label: 'World' },
-    position: { x: 100, y: 100 },
-  },
-];
-
-const edges: Edge[] = [{ id: '1-2', source: '1', target: '2' }];
-
 const Flow = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds: Node[]) => applyNodeChanges(changes, nds)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds: Edge[]) => applyEdgeChanges(changes, eds)),
+    [],
+  );
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) =>
+      setEdges((eds: Edge[]) => addEdge(params, eds)),
+    [],
+  );
+
   return (
     <div style={{ height: '100vh' }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
