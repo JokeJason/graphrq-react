f76c0230a91a3561376204c6fc6e761b14ba4979
Jason Zhu <jasonzhuyq@outlook.com>
2023-08-20 11:24:05 +1000

    Trying to refactor code following reactflow example

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import ReactFlow, { Background, Controls, NodeTypes } from 'reactflow';
+import ReactFlow, { Background, Controls, Edge, Node } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-import RequirementNode from './components/RequirementNode.tsx';
-
-const nodes = [
+const initialNodes: Node[] = [
   {
     id: '1',
-    position: { x: 0, y: 0 },
     data: { label: 'Hello' },
-    type: 'requirement',
+    position: { x: 0, y: 0 },
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: { label: 'World' },
+    position: { x: 100, y: 100 },
   },
 ];
 
-const nodeTypes: NodeTypes = { requirement: RequirementNode };
+const initialEdges: Edge[] = [{ id: '1-2', source: '1', target: '2' }];
+
+const nodes: Node[] = [
+  {
+    id: '1',
+    data: { label: 'Hello' },
+    position: { x: 0, y: 0 },
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: { label: 'World' },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const edges: Edge[] = [{ id: '1-2', source: '1', target: '2' }];
 
 const Flow = () => {
   return (
     <div style={{ height: '100vh' }}>
-      <ReactFlow nodes={nodes} nodeTypes={nodeTypes}>
+      <ReactFlow nodes={nodes} edges={edges}>
         <Background />
         <Controls />
       </ReactFlow>
