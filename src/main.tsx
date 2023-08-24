11ad2be783eb84446e68b6ff14479ed413fa5384
Jason Zhu <jasonzhuyq@outlook.com>
2023-08-24 23:32:52 +1000

    Use Redux toolkit for state management (fix Provider)

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ReactFlowProvider } from 'reactflow';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ReactFlowProvider>
-      <App />
-    </ReactFlowProvider>
+    <Provider store={store}>
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
+    </Provider>
   </React.StrictMode>,
 );
