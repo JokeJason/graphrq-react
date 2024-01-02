53767fb85c748dcb8eb69ea622564b962350c299
Jason Zhu <jasonzhuyq@outlook.com>
2024-01-02 14:09:51 +1100

    Implement util methods to convert requirement from graphrq endpoint to reactflow node and tested it

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
+  },
+  testRegex: '(/__tests__/.*\\.(test|spec))\\.(m)?ts$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.test.ts',
+    'src/**/*.mts',
+    '!src/**/*.d.ts',
+    '!src/**/*.d.mts',
+    '!src/__tests__/**/*',
+  ],
+};
---
+++ src/__tests__/utils.test.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import * as utils from '../utils';
+import { NodeType } from '../types.ts';
+
+describe('graphrq-react test', () => {
+  test('createReactFlowNodes should convert json to NodeType', () => {
+    const rawContent = fs.readFileSync('src/__tests__/requirements.json', {
+      encoding: 'utf8',
+    });
+
+    const jsonData = JSON.parse(rawContent);
+    const nodeType = utils.createReactFlowNodes(jsonData.requirements);
+
+    const matchedData0: NodeType = {
+      id: '93fed700-ea33-43c5-8946-a88572850a5e',
+      data: {
+        title: 'Req1',
+        description: 'Description of Req1',
+      },
+      position: { x: 0, y: 0 },
+      type: 'requirement',
+    };
+    const matchedData1: NodeType = {
+      id: 'ce11a71d-f075-4752-a3cd-d7353bd60b95',
+      data: {
+        title: 'Req1.1-Updated',
+        description: 'Description of Req1.1-Updated',
+      },
+      position: { x: 0, y: 0 },
+      type: 'requirement',
+    };
+    expect(nodeType[0]).toEqual(matchedData0);
+    expect(nodeType[1]).toEqual(matchedData1);
+  });
+});
