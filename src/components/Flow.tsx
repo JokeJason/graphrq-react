bfd52ca27d9ca0311ba95d8ef6c1e1e0a28f082c
Jason Zhu <jasonzhuyq@outlook.com>
2024-01-02 21:54:50 +1100

    Successfully implemented dagre layout

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -2,6 +2,7 @@ import { gql, OperationVariables, QueryResult, useQuery } from '@apollo/client';
 import * as utils from '../utils';
 import ReactFlow, { Background, Controls, NodeTypes } from 'reactflow';
 import RequirementNode from './RequirementNode.tsx';
+import dagre from '@dagrejs/dagre';
 
 const GET_REQUIREMENTS = gql(`
   query Requirements {
@@ -18,6 +19,42 @@ const GET_REQUIREMENTS = gql(`
 
 const nodeTypes: NodeTypes = { requirement: RequirementNode };
 
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWith = 500;
+const nodeHeight = 100;
+
+const getLayoutedElements = (nodes: any[], edges: any[], direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach(node => {
+    dagreGraph.setNode(node.id, { width: nodeWith, height: nodeHeight });
+  });
+
+  edges.forEach(edge => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach(node => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? 'left' : 'top';
+    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+    node.position = {
+      x: nodeWithPosition.x - nodeWith / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
 const Flow = () => {
   const { loading, error, data }: QueryResult<any, OperationVariables> =
     useQuery(GET_REQUIREMENTS);
@@ -25,19 +62,15 @@ const Flow = () => {
   if (loading) return <p> Loading... </p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const nodes = utils.createReactFlowNodes(data.requirements);
-  const edges = utils.createReactFlowEdges(data.requirements);
+  const { nodes, edges } = getLayoutedElements(
+    utils.createReactFlowNodes(data.requirements),
+    utils.createReactFlowEdges(data.requirements),
+    'LR',
+  );
 
   return (
     <div style={{ height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        onNodesChange={change => console.log(change)}
-        edges={edges}
-        onEdgesChange={change => console.log(change)}
-        nodeTypes={nodeTypes}
-        fitView
-      >
+      <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes} fitView>
         <Background />
         <Controls />
       </ReactFlow>
